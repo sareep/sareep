bc899fe2ecd346b35fce89faa913a647d48c7275
Sam Reep <reepsaml@gmail.com>
2025-07-08 20:29:20 -0700

    basic layout

---
+++ src/app/header.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Button,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
+export default function Header() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="sticky" >
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Sam Reep
+        </Typography>
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MenuIcon />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          slotProps={{
+            list: {
+              "aria-labelledby": "basic-button",
+            },
+          }}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,90 @@
+import { Card, Grid, Typography } from "@mui/material";
+import Header from "./header";
+
+const MENU_OPTIONS = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Projects", href: "/projects" },
+  { label: "Contact", href: "/contact" },
+];
+
+const COLUMN_WIDTHS = {
+  left: 3,
+  right: 9,
+};
+
+function renderColumnGridItem(
+  children: React.ReactNode,
+  side: keyof typeof COLUMN_WIDTHS,
+  noCard: boolean = false
+) {
+  return (
+    <Grid
+      container
+      size={COLUMN_WIDTHS[side]}
+      alignContent={"center"}
+      justifyContent={"center"}
+    >
+      {noCard ? (
+        children
+      ) : (
+        <Card variant="elevation" sx={{ p: 2, width: "100%", height: "100%" }}>
+          {children}
+        </Card>
+      )}
+    </Grid>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen ">
-      {/* TODO header with menu button */}
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <img
-            src="/headshot.jpeg"
-            width={2100/8}
-            height={1925/8}
-          />
-          {/* TODO paragraph about me */}
-      </div>
-
-      <div className="flex flex-col items-center justify-center p-6 md:w-2/5 md:px-28 md:py-12">
-        <h1 className="text-4xl font-bold mb-4">Hello, I'm [Your Name]</h1>
-        <p className="text-lg text-gray-700">
-          I am a [Your Profession/Role] with a passion for [Your Interests]. 
-          Welcome to my personal website!
-        </p>
-        {/* TODO quick links */}
-        
-      </div>
-
+    <main>
+      <Header />
+      <Grid container py={1} mx={2} spacing={2}>
+        {renderColumnGridItem(
+          <div>
+            <img
+              alt="Sam Reep"
+              src="/headshot.jpeg"
+              style={{
+                aspectRatio: "210 / 195",
+                maxWidth: "100%",
+                borderRadius: "50%",
+                overflow: "hidden",
+              }}
+            />
+          </div>,
+          "left",
+          true
+        )}
+        {renderColumnGridItem(
+          <Typography variant="body1" gutterBottom>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Typography>,
+          "right"
+        )}
+      </Grid>
+      <Grid container py={1} mx={2} spacing={2}>
+        {renderColumnGridItem(
+          <Typography variant="h4" gutterBottom>
+            Quick Links
+          </Typography>,
+          "left"
+        )}
+        {renderColumnGridItem(
+          <Grid size={8}>
+            {/* TODO */}
+            Resume Here
+          </Grid>,
+          "right"
+        )}
+      </Grid>
     </main>
   );
 }

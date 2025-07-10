a32abd3bb40709a71189f11cc23e4889c5cb0739
Sam Reep <reepsaml@gmail.com>
2025-07-09 21:21:13 -0700

    fix header layout

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -22,30 +22,48 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="sticky" >
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    <AppBar
+      position="sticky"
+      // elevation={3}
+      sx={{
+        width: "97.5%",
+        m: 2,
+        borderRadius: 4,
+        boxSizing: "border-box",
+        overflow: "visible",
+        backgroundColor: "background.header",
+        color: "text.primary",
+      }}
+    >
+      <Toolbar
+        sx={{
+          px: { xs: 2, sm: 4 },
+        }}
+      >
+        <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
           Sam Reep
         </Typography>
         <Button
           id="basic-button"
+          color="inherit"
           aria-controls={open ? "basic-menu" : undefined}
           aria-haspopup="true"
           aria-expanded={open ? "true" : undefined}
           onClick={handleClick}
         >
           <MenuIcon />
+          <Typography
+            variant="button"
+            sx={{ display: { xs: "none", sm: "inline" }, ml: 1 }}
+          >
+            Menu
+          </Typography>
         </Button>
         <Menu
           id="basic-menu"
           anchorEl={anchorEl}
           open={open}
           onClose={handleClose}
-          slotProps={{
-            list: {
-              "aria-labelledby": "basic-button",
-            },
-          }}
         >
           <MenuItem onClick={handleClose}>Profile</MenuItem>
           <MenuItem onClick={handleClose}>My account</MenuItem>
